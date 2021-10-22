7e12e0ef9f61cbc46a866607fa018bfcd968da4b
Diego Rozo Herrera <drozohe@gmail.com>
2021-10-22 11:32:07 -0500

    Agrego servicio para guardar un nuevo documento dentro de la coleccion de cloud firestore con el metodo POST

--- src/main/java/com/kenobee/TaskManagement/controller/TaskController.java
+++ src/main/java/com/kenobee/TaskManagement/controller/TaskController.java
@@ -1,8 +1,15 @@
 
 package com.kenobee.TaskManagement.controller;
 
+import com.kenobee.TaskManagement.dto.TaskDTO;
+import com.kenobee.TaskManagement.service.TaskManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/tasks")
 public class TaskController {
     
+    //[drozo] inyectar el servicio dentro del controlador, a traves de la interface accedo a la implementación.
+    @Autowired
+    private TaskManagementService service;
+    
     @GetMapping(value = "/greet/{name}")
     public String greet(@PathVariable(value = "name") String name){
         return  "Hello, "+name;
+    }
+    
+    @PostMapping(value = "/add")
+    public ResponseEntity add(@RequestBody TaskDTO post){
+        return new ResponseEntity(service.add(post), HttpStatus.OK);
     }
 }
--- src/main/java/com/kenobee/TaskManagement/service/impl/TaskManagementServiceImpl.java
+++ src/main/java/com/kenobee/TaskManagement/service/impl/TaskManagementServiceImpl.java
@@ -1,10 +1,14 @@
-
 package com.kenobee.TaskManagement.service.impl;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.WriteResult;
 import com.kenobee.TaskManagement.firebase.FireBaseInitializer;
 import com.kenobee.TaskManagement.dto.TaskDTO;
 import com.kenobee.TaskManagement.service.TaskManagementService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +16,39 @@ import org.springframework.stereotype.Service;
  *
  * @author drozohe
  */
-
 @Service
-public class TaskManagementServiceImpl implements TaskManagementService{
+public class TaskManagementServiceImpl implements TaskManagementService {
 
+    //[drozo] Se encarga de inyectar una interface, o un servicio y administrar sus instancias
+    //        para manejo de memoria. Se encarga de crear una unica instancia dentro de este servicio.
     @Autowired
     private FireBaseInitializer firebase;
-    
+
     @Override
     public List<TaskDTO> list() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    
     @Override
     public Boolean add(TaskDTO post) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        //Se crea un mapa para obtener los datos del documento (llave valor)
+        Map<String, Object> docData = getDocData(post);
+
+        //[escribir un documento dentro de la colección]    
+        ApiFuture<WriteResult> writeResultApiFuture = getCollection().document().create(docData);
+
+        try {
+            //[drozo] Verificar si fue posible escribir el documento
+            if (writeResultApiFuture.get() != null ) {
+                return Boolean.TRUE;
+            }
+            return Boolean.FALSE;
+        } catch (Exception e) { //[drozo] Se prodrian manejar excepciones expecificas. Por ahora una generica...
+            return Boolean.FALSE;
+        }
+
     }
 
     @Override
@@ -38,6 +60,22 @@ public class TaskManagementServiceImpl implements TaskManagementService{
     public Boolean delete(String id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
+
+    //[drozo] Obtener la coleccion de cloudstore
+    private CollectionReference getCollection() {
+        return firebase.getFirestore().collection("task");
+    }
+
+    //[drozo] 
+    private Map<String, Object> getDocData(TaskDTO task) {
+        
+        Map<String, Object> docData = new HashMap<>();
+        
+        //
+        docData.put("title", task.getTitle());
+        docData.put("content", task.getContent());
+        
+        return docData;
+    }
+
 }
