7b1fcaa780e603af5874e8c52e3b84fb9b3a229f
Diego Rozo Herrera <drozohe@gmail.com>
2021-10-21 23:14:24 -0500

    Agrego paquete DTO y archivo TaskDTO.java

---
+++ src/main/java/com/kenobee/TaskManagement/dto/TaskDTO.java
@@ -0,0 +1,16 @@
+
+package com.kenobee.TaskManagement.dto;
+
+import lombok.Data;
+
+/**
+ *
+ * @author drozohe
+ */
+
+@Data
+public class TaskDTO {
+    private String id;
+    private String title;
+    private String content;
+}
